cc87cd83af4ca66e525dbff281e49cb1dd8d1f70
Hongxu Li <1073194628@qq.com>
2019-04-17 20:13:33 +0800

    主界面

--- app/src/main/java/com/example/lhx/recovery/main_interface.java
+++ app/src/main/java/com/example/lhx/recovery/main_interface.java
@@ -2,6 +2,7 @@ package com.example.lhx.recovery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class main_interface extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class main_interface extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_interface);
+
+
     }
 }
