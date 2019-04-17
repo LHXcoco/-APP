7494c088cd3a75abe3ef350420ecb0c4589e052f
Hongxu Li <1073194628@qq.com>
2019-04-17 21:35:33 +0800

    登录和注册界面

--- app/src/main/java/com/example/lhx/recovery/main_interface.java
+++ app/src/main/java/com/example/lhx/recovery/main_interface.java
@@ -1,16 +1,30 @@
 package com.example.lhx.recovery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class main_interface extends AppCompatActivity {
+
+    private Button t_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_interface);
 
+        //测试按钮点击事件
+        t_btn = (Button)findViewById(R.id.testbtn);
+        t_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main_interface.this,LoadActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
